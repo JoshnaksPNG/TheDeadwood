1c6fc7875c7c6c0674345d8a37d3e3c2cbfa5948
JoshnaksPNG <80712017+JoshnaksPNG@users.noreply.github.com>
2025-02-17 17:51:32 -0800

    Implemented Dice.java
    Implemented Dice::rollMulti
    Implemented Dice::roll

--- src/main/java/org/model/Dice.java
+++ src/main/java/org/model/Dice.java
@@ -2,22 +2,55 @@ package org.model;
 
 import jdk.jshell.spi.ExecutionControl;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Random;
+
 public class Dice
 {
-    int result;
+    public int result;
 
-    int[] results;
+    public int[] results;
+
+    private Random _random;
+
+    private static final int _DICELOW = 1;
+
+    private static final int _DICEHIGH = 6;
 
     Dice()
-    {}
-
-    int roll() throws ExecutionControl.NotImplementedException
     {
-        throw new ExecutionControl.NotImplementedException("Method Not Implemented");
+        long seed = new Date().getTime();
+        _random = new Random(seed);
+
+        results = new int[0];
+    }
+
+    int roll()
+    {
+        int res = _random.nextInt(_DICELOW, _DICEHIGH + 1);
+
+        result = res;
+
+        return res;
     }
 
     int[] rollMulti(int rollCount) throws ExecutionControl.NotImplementedException
     {
-        throw new ExecutionControl.NotImplementedException("Method Not Implemented");
+        results = new int[rollCount];
+
+        ArrayList<Integer> res = new ArrayList<>();
+
+        for(int i = 0; i < rollCount; ++i)
+        {
+            res.add(roll());
+        }
+
+        for(int i = 0; i < rollCount; ++i)
+        {
+            results[i] = res.get(i);
+        }
+
+        return results.clone();
     }
 }
