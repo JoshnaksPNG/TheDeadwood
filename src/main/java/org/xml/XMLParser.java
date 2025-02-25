22081eec51cbf5cfc69d6a5f26c07e3e4a95c5ca
Richard <wangr6@wwu.edu>
2025-02-25 09:09:56 -0800

    Starting XMLParser for board data

--- src/main/java/org/xml/XMLParser.java
+++ src/main/java/org/xml/XMLParser.java
@@ -88,7 +88,55 @@ public class XMLParser{
       return scenes;
    } // method
 
+   public void readBoardData(Document d){
+      Element root = d.getDocumentElement();
+      NodeList cards = root.getElementsByTagName("board");
+      Role roleHolder = null;
+      ArrayList<Role> roles = new ArrayList<Role>();
+      for (int i=0; i < cards.getLength();i++){
+         System.out.println("Printing information for Scene "+(i+1));
+         
+         //reads data from the nodes
+         Node card = cards.item(i);
+         String name = card.getAttributes().getNamedItem("name").getNodeValue();
+         System.out.println("name = "+ name);
+         
+         //reads data
+         NodeList children = card.getChildNodes();
+         int sceneNum = 0;
+         String sceneText = "";
+         String partName = "";
+         int level = 0;
+         String partLine = "";
+         int budget = Integer.parseInt(card.getAttributes().getNamedItem("budget").getNodeValue());
+         for (int j=0; j < children.getLength(); j++){
+            Node sub = children.item(j);
+            if("scene".equals(sub.getNodeName())){
+               sceneNum = Integer.parseInt(sub.getAttributes().getNamedItem("number").getNodeValue());
+               sceneText = sub.getTextContent();
+               System.out.println("Scene # = "+sceneNum);
+               System.out.println("Scene text = " + sceneText);
+            } else if("part".equals(sub.getNodeName())){
+               partName = sub.getAttributes().getNamedItem("name").getNodeValue();
+               System.out.println("part name = "+partName);
+               level = Integer.parseInt(sub.getAttributes().getNamedItem("level").getNodeValue());
+               System.out.println("part level = "+level);
 
+               NodeList partChildren = sub.getChildNodes();
+               for (int k = 0; k < partChildren.getLength(); k++) {
+                  Node partSub = partChildren.item(k);
+                  if ("line".equals(partSub.getNodeName())) {
+                      partLine = partSub.getTextContent();
+                  }
+               }
+               System.out.println("part line = "+partLine);
+               roleHolder = new Role(partName, partLine, level, true);
+            }
+            roles.add(roleHolder);
+         } //for childnodes
+      } //for card node
+      
+   } // method
 
 
 
