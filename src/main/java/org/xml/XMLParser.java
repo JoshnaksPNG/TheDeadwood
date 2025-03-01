d8fa32625c513e7894dd77eb0492513413302865
JoshnaksPNG <80712017+JoshnaksPNG@users.noreply.github.com>
2025-02-28 23:20:23 -0800

    Update XMLParser.java

--- src/main/java/org/xml/XMLParser.java
+++ src/main/java/org/xml/XMLParser.java
@@ -40,18 +40,17 @@ public class XMLParser{
       Role roleHolder = null;
       Scene sceneHolder = null;
       ArrayList<Role> roles = new ArrayList<Role>();
-      System.out.println("Number of elements = " + cards.getLength());
+
       for (int i = 0; i < cards.getLength(); i++){
-         System.out.println("Printing information for Scene "+(i+1));
          
          //reads data from the nodes
          Node card = cards.item(i);
          String name = card.getAttributes().getNamedItem("name").getNodeValue();
-         System.out.println("name = "+ name);
+
          String img = card.getAttributes().getNamedItem("img").getNodeValue();
-         System.out.println("img = " + img);
+
          int budget = Integer.parseInt(card.getAttributes().getNamedItem("budget").getNodeValue());
-         System.out.println("buget = " + budget);
+
 
          //reads data
          NodeList children = card.getChildNodes();
@@ -66,31 +65,30 @@ public class XMLParser{
 
             if("scene".equals(sub.getNodeName())){
                sceneNum = Integer.parseInt(sub.getAttributes().getNamedItem("number").getNodeValue());
-               System.out.println("Scene # = "+sceneNum);
+
                sceneText = sub.getTextContent();
-               System.out.println("Scene text = " + sceneText);
 
             } else if("part".equals(sub.getNodeName())){
                partName = sub.getAttributes().getNamedItem("name").getNodeValue();
-               System.out.println("part name = "+partName);
+
                partLevel = Integer.parseInt(sub.getAttributes().getNamedItem("level").getNodeValue());
-               System.out.println("part level = " + partLevel);
+
 
                NodeList partChildren = sub.getChildNodes();
                for (int k = 0; k < partChildren.getLength(); k++) {
                   Node partSub = partChildren.item(k);
                   if ("area".equals(partSub.getNodeName())) {
                      String partX = partSub.getAttributes().getNamedItem("x").getNodeValue();
-                     System.out.print("x=" + partX);
+
                      String partY = partSub.getAttributes().getNamedItem("y").getNodeValue();
-                     System.out.print(" y=" + partY);
+
                      String partH = partSub.getAttributes().getNamedItem("h").getNodeValue();
-                     System.out.print(" h=" + partH);
+
                      String partW = partSub.getAttributes().getNamedItem("w").getNodeValue();
-                     System.out.println(" w=" + partW);
+
                   } else if ("line".equals(partSub.getNodeName())) {
                       partLine = partSub.getTextContent();
-                      System.out.println("part line = " + partLine);
+
                   }
                }
                
@@ -125,14 +123,12 @@ public class XMLParser{
       ArrayList<String> neighbors = new ArrayList<String>();
 
       for (int i = 0; i < sets.getLength(); i++) {
-         System.out.println("Printing information for set " + (i + 1));
 
          Node set = sets.item(i);
          NodeList children = set.getChildNodes();
          setName = set.getAttributes().getNamedItem("name").getNodeValue();
          // ArrayList<String> neighbors = new ArrayList<String>();
-         
-         System.out.println("Set Name = " + setName);
+
 
          for (int j = 0; j < children.getLength(); j++) {
             Node sub = children.item(j);
@@ -146,18 +142,17 @@ public class XMLParser{
                   if ("neighbor".equals(neighborSub.getNodeName())) {
                      String neighbor = neighborSub.getAttributes().getNamedItem("name").getNodeValue();
                      neighbors.add(neighbor);
-                     System.out.println("neighbor = " + neighbor);
                   }
                }
             } else if ("area".equals(sub.getNodeName())) {
                sceneX = Integer.parseInt(sub.getAttributes().getNamedItem("x").getNodeValue());
-               System.out.print("Scene Dimensions: x = " + sceneX);
+
                sceneY = Integer.parseInt(sub.getAttributes().getNamedItem("y").getNodeValue());
-               System.out.print(", y = " + sceneY);
+
                sceneH = Integer.parseInt(sub.getAttributes().getNamedItem("h").getNodeValue());
-               System.out.print(", h = " + sceneH);
+
                sceneW = Integer.parseInt(sub.getAttributes().getNamedItem("w").getNodeValue());
-               System.out.println(", w =" + sceneW);
+
             } else if ("takes".equals(sub.getNodeName())) {
                NodeList takeChildren = sub.getChildNodes();
                for (int k = 0; k < takeChildren.getLength(); k++) {
@@ -165,7 +160,6 @@ public class XMLParser{
 
                   if ("take".equals(takeSub.getNodeName())) {
                      String numTakes = takeSub.getAttributes().getNamedItem("number").getNodeValue();
-                     System.out.println("takes = " + numTakes);
 
                      NodeList takeGrandchildren = takeSub.getChildNodes();
                      for (int h = 0; h < takeGrandchildren.getLength(); h++) {
@@ -173,13 +167,13 @@ public class XMLParser{
                         if ("area".equals(takeSubSub.getNodeName())) {
                            takes++;
                            String takeX = takeSubSub.getAttributes().getNamedItem("x").getNodeValue();
-                           System.out.print("Take Dimensions: x = " + takeX);
+
                            String takeY = takeSubSub.getAttributes().getNamedItem("y").getNodeValue();
-                           System.out.print(", y = " + takeY);
+
                            String takeH = takeSubSub.getAttributes().getNamedItem("h").getNodeValue();
-                           System.out.print(", h = " + takeH);
+
                            String takeW = takeSubSub.getAttributes().getNamedItem("w").getNodeValue();
-                           System.out.println(", w = " + takeW);
+
                         }
                      }
                   }
@@ -192,22 +186,22 @@ public class XMLParser{
 
                   if ("part".equals(partSub.getNodeName())) {
                      partName = partSub.getAttributes().getNamedItem("name").getNodeValue();
-                     System.out.println("partName = " + partName);
+
                      partLevel = Integer.parseInt(partSub.getAttributes().getNamedItem("level").getNodeValue());
-                     System.out.println("partLvl  = " + partLevel);
+
 
                      NodeList partGrandchildren = partSub.getChildNodes();
                      for (int l = 0; l < partGrandchildren.getLength(); l++) {
                         Node partSubSub = partGrandchildren.item(l);
                         if ("area".equals(partSubSub.getNodeName())) {
                            partX = Integer.parseInt(partSubSub.getAttributes().getNamedItem("x").getNodeValue());
-                           System.out.print("Part Dimensions: x = " + partX);
+
                            partY = Integer.parseInt(partSubSub.getAttributes().getNamedItem("y").getNodeValue());
-                           System.out.print(", y = " + partY);
+
                            partH = Integer.parseInt(partSubSub.getAttributes().getNamedItem("h").getNodeValue());
-                           System.out.print(", h = " + partH);
+
                            partW = Integer.parseInt(partSubSub.getAttributes().getNamedItem("w").getNodeValue());
-                           System.out.println(", w = " + partW);
+
 
                         } else if ("line".equals(partSubSub.getNodeName())) {
                            line = partSubSub.getTextContent();
@@ -232,15 +226,15 @@ public class XMLParser{
             
          }
          rooms[i] = new Set(setName, sceneX, sceneY, sceneW, sceneH, neighbors, null, roles, takes);
-         System.out.println("Size of roles = " + roles.size());
+
          neighbors.clear();
          roles.clear();
          
          System.out.println();
       }
-      System.out.println("Printing information for trailer.");
+
       // creating a trailer room specially at index 0 of the rooms array
-      System.out.println("Printing information for trailer.");
+
       Node tr = trailer.item(0);
       NodeList trChildren = tr.getChildNodes();
       for (int t = 0; t < trChildren.getLength(); t++) {
@@ -257,19 +251,17 @@ public class XMLParser{
                      neighbors.add(neighbor);
                   }
                }
-               System.out.println("Number of neighbors = " + neighbors.size());
-               System.out.println("Containing: " + neighbors.toString());
                // neighbors.clear();
 
          } else if ("area".equals(sub.getNodeName())) {
                sceneX = Integer.parseInt(sub.getAttributes().getNamedItem("x").getNodeValue());
-               System.out.print("Scene Dimensions: x = " + sceneX);
+
                sceneY = Integer.parseInt(sub.getAttributes().getNamedItem("y").getNodeValue());
-               System.out.print(", y = " + sceneY);
+
                sceneH = Integer.parseInt(sub.getAttributes().getNamedItem("h").getNodeValue());
-               System.out.print(", h = " + sceneH);
+
                sceneW = Integer.parseInt(sub.getAttributes().getNamedItem("w").getNodeValue());
-               System.out.println(", w =" + sceneW);
+
                
          }
       }
@@ -289,19 +281,17 @@ public class XMLParser{
                   neighbors.add(neighbor);
                }
             }
-            System.out.println("Number of neighbors = " + neighbors.size());
-            System.out.println("Containing neighbors: " + neighbors.toString());
             // neighbors.clear();
 
          } else if ("area".equals(sub.getNodeName())) {
             sceneX = Integer.parseInt(sub.getAttributes().getNamedItem("x").getNodeValue());
-            System.out.print("Scene Dimensions: x = " + sceneX);
+
             sceneY = Integer.parseInt(sub.getAttributes().getNamedItem("y").getNodeValue());
-            System.out.print(", y = " + sceneY);
+
             sceneH = Integer.parseInt(sub.getAttributes().getNamedItem("h").getNodeValue());
-            System.out.print(", h = " + sceneH);
+
             sceneW = Integer.parseInt(sub.getAttributes().getNamedItem("w").getNodeValue());
-            System.out.println(", w =" + sceneW);
+
          }
       }
       rooms[11] = new CastingOffice("office", sceneX, sceneY, sceneW, sceneH, neighbors);
