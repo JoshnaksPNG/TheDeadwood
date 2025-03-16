1a5ad277aa7977554b3b10d7b5dd151be6d9f9c3
Richard <wangr6@wwu.edu>
2025-03-16 11:47:53 -0700

    Cleaned up placeing takes

--- src/main/java/org/view/BoardLayersListener.java
+++ src/main/java/org/view/BoardLayersListener.java
@@ -330,21 +330,11 @@ public class BoardLayersListener extends JFrame implements IView
            JLabel take = new JLabel();
            take.setIcon(shot);
            take.setBounds(arr[0], arr[1], arr[3], arr[2]);
-           take.setOpaque(true);
+           take.setOpaque(false);
            bPane.add(take, Integer.valueOf(4));
            takes.add(take);
         }  
         s.setTakes(takes);
-        // // LogText("Fail");
-        // s.printTakesList();
-        // ArrayList<JLabel> takes = new ArrayList<>();
-        // LogText("" + takeList.size());
-        // take = new JLabel();
-        // take.setIcon(shot);
-        // take.setBounds(s.getX(),s.getY(),shot.getIconWidth(),shot.getIconHeight());
-        // take.setOpaque(true);
-        // // Add the card back on top of card
-        // bPane.add(take, Integer.valueOf(4));
     }
 
     public void promptRolesSelections(Player player) {
